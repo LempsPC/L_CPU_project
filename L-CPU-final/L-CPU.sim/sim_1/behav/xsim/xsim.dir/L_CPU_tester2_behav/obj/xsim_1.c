5625efbed91666f2a95e330beab2027cadd352c4
LempsPC <lemps5245@gmail.com>
2018-05-13 19:01:46 +0300

    tegin alamklassi, mis programme jooksutab, kasutades peaklassi meetodeid neat

--- L-CPU-final/L-CPU.sim/sim_1/behav/xsim/xsim.dir/L_CPU_tester2_behav/obj/xsim_1.c
+++ L-CPU-final/L-CPU.sim/sim_1/behav/xsim/xsim.dir/L_CPU_tester2_behav/obj/xsim_1.c
@@ -45,41 +45,42 @@
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
 extern void execute_2(char*, char *);
-extern void execute_68(char*, char *);
 extern void execute_69(char*, char *);
-extern void execute_74(char*, char *);
-extern void execute_61(char*, char *);
+extern void execute_70(char*, char *);
+extern void execute_75(char*, char *);
 extern void execute_62(char*, char *);
-extern void execute_66(char*, char *);
+extern void execute_63(char*, char *);
 extern void execute_67(char*, char *);
-extern void execute_64(char*, char *);
+extern void execute_68(char*, char *);
 extern void execute_65(char*, char *);
-extern void execute_71(char*, char *);
+extern void execute_66(char*, char *);
 extern void execute_72(char*, char *);
 extern void execute_73(char*, char *);
-extern void execute_75(char*, char *);
+extern void execute_74(char*, char *);
 extern void execute_76(char*, char *);
 extern void execute_77(char*, char *);
 extern void execute_78(char*, char *);
 extern void execute_79(char*, char *);
-extern void transaction_11(char*, char*, unsigned, unsigned, unsigned);
+extern void execute_80(char*, char *);
+extern void transaction_17(char*, char*, unsigned, unsigned, unsigned);
 extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-extern void transaction_13(char*, char*, unsigned, unsigned, unsigned);
-extern void transaction_30(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_19(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_36(char*, char*, unsigned, unsigned, unsigned);
 extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
 extern void transaction_0(char*, char*, unsigned, unsigned, unsigned);
 extern void transaction_1(char*, char*, unsigned, unsigned, unsigned);
 extern void transaction_3(char*, char*, unsigned, unsigned, unsigned);
 extern void transaction_4(char*, char*, unsigned, unsigned, unsigned);
 extern void transaction_5(char*, char*, unsigned, unsigned, unsigned);
-funcp funcTab[28] = {(funcp)execute_2, (funcp)execute_68, (funcp)execute_69, (funcp)execute_74, (funcp)execute_61, (funcp)execute_62, (funcp)execute_66, (funcp)execute_67, (funcp)execute_64, (funcp)execute_65, (funcp)execute_71, (funcp)execute_72, (funcp)execute_73, (funcp)execute_75, (funcp)execute_76, (funcp)execute_77, (funcp)execute_78, (funcp)execute_79, (funcp)transaction_11, (funcp)vhdl_transfunc_eventcallback, (funcp)transaction_13, (funcp)transaction_30, (funcp)vlog_transfunc_eventcallback, (funcp)transaction_0, (funcp)transaction_1, (funcp)transaction_3, (funcp)transaction_4, (funcp)transaction_5};
-const int NumRelocateId= 28;
+extern void vlog_transfunc_eventcallback_2state(char*, char*, unsigned, unsigned, unsigned, char *);
+funcp funcTab[29] = {(funcp)execute_2, (funcp)execute_69, (funcp)execute_70, (funcp)execute_75, (funcp)execute_62, (funcp)execute_63, (funcp)execute_67, (funcp)execute_68, (funcp)execute_65, (funcp)execute_66, (funcp)execute_72, (funcp)execute_73, (funcp)execute_74, (funcp)execute_76, (funcp)execute_77, (funcp)execute_78, (funcp)execute_79, (funcp)execute_80, (funcp)transaction_17, (funcp)vhdl_transfunc_eventcallback, (funcp)transaction_19, (funcp)transaction_36, (funcp)vlog_transfunc_eventcallback, (funcp)transaction_0, (funcp)transaction_1, (funcp)transaction_3, (funcp)transaction_4, (funcp)transaction_5, (funcp)vlog_transfunc_eventcallback_2state};
+const int NumRelocateId= 29;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/L_CPU_tester2_behav/xsim.reloc",  (void **)funcTab, 28);
-	iki_vhdl_file_variable_register(dp + 18656);
-	iki_vhdl_file_variable_register(dp + 18712);
+	iki_relocate(dp, "xsim.dir/L_CPU_tester2_behav/xsim.reloc",  (void **)funcTab, 29);
+	iki_vhdl_file_variable_register(dp + 19856);
+	iki_vhdl_file_variable_register(dp + 19912);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
@@ -92,11 +93,11 @@ void sensitize(char *dp)
 
 void simulate(char *dp)
 {
-iki_register_root_pointers(1, 13864, 1,0) ; 
+iki_register_root_pointers(2, 14032, 2,0,14200, 3,0) ; 
 	iki_schedule_processes_at_time_zero(dp, "xsim.dir/L_CPU_tester2_behav/xsim.reloc");
 	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
 
-	iki_vlog_schedule_transaction_signal_fast_vhdl_value_time_0(dp + 23064, dp + 21144, 0, 4, 0, 4, 5, 1);
+	iki_vlog_schedule_transaction_signal_fast_vhdl_value_time_0(dp + 24600, dp + 22456, 0, 4, 0, 4, 5, 1);
 	iki_execute_processes();
 
 	// Schedule resolution functions for the multiply driven Verilog nets that have strength
--- L-CPU-final/L-CPU.srcs/sim_1/new/L_CPU_tester2.sv
+++ L-CPU-final/L-CPU.srcs/sim_1/new/L_CPU_tester2.sv
@@ -13,30 +13,75 @@ reg [7:0] Aadress_to_ram_debug;
 
 always #5 clk ++;
 
-class Instruction;
-	/*--Test classes have 'test' task's that will check CPU status  */
+int memoryposition = 0;
+
+class Memory;
+	
 	int instcount = 0;
 	/*---------------execute---------------*/
-	task insertCommand(input reg[7:0]command1, command2 );
+	//this task inserts direct loading command with corresponding operand and data which can be loaded into desired register
+	//this task starts from falling edge ans ends at falling edge, lasts 2 cycles
+	task insertDataDirect(input reg[7:0] dataToinsert, input int operand );
 	debug <= 1;
 	Aadress_to_ram_debug <= instcount;
-	Data_to_ram_debug <= command1;
+	Data_to_ram_debug <= (16 + operand); //first command inserted, which tells to load from next register
 	@(posedge clk); //wait for positive edge of clk
 	Aadress_to_ram_debug <= (instcount +1);
 	instcount++;
-	Data_to_ram_debug <= command2;
+	Data_to_ram_debug <= dataToinsert; //inserting data into register so CPU could read it
+	$display ("@%g Inserted data %d into register %d", $time, (16 + operand), (instcount -1));
 	@(posedge clk);
 	@(negedge clk);//wait for negative edge of clock
 	debug <= 0;
 	Data_to_ram_debug <= 0;
     Aadress_to_ram_debug <= 0;
     instcount++;	
-	$display ("@%g Value passed is %d", $time, command2);
+	$display ("@%g Inserted data %d into register %d", $time, dataToinsert, (instcount -1));
 	endtask;
-endclass: Instruction
+	
+	//this task starts from falling edge ans ends at falling edge, lasts 1 cycle
+	task insertIntoCell(input reg[7:0] aadressToInsert, dataToInsert);
+	debug <= 1;
+	Aadress_to_ram_debug <= aadressToInsert;
+	Data_to_ram_debug <= dataToInsert;
+	@(posedge clk);
+	@(negedge clk);
+	$display ("@%g Inserted data %d into register %d", $time, dataToInsert, aadressToInsert);
+	debug <= 0;
+	endtask;
+	
+	
+	
+endclass: Memory;
+
+
+class Testprogram extends Memory;
+    
+    task DirectDataInsert(input reg[7:0] dataToinsert, input int operand);
+        insertIntoCell(memoryposition, (16+operand) );
+        memoryposition ++;
+        insertIntoCell(memoryposition, dataToinsert);
+        memoryposition++;
+    endtask;
+    
+    task adding(input reg[7:0] operand1, operand2);
+        insertIntoCell(memoryposition, 17);
+        memoryposition++;
+        insertIntoCell(memoryposition, operand1);
+        memoryposition++;
+        insertIntoCell(memoryposition, 18);
+        memoryposition++;
+        insertIntoCell(memoryposition, operand2);
+        memoryposition++;
+        insertIntoCell(memoryposition, 163); //adding command
+        memoryposition++;
+    endtask;
+    
+endclass: Testprogram;
+
 
 Instruction laadimine1 = new;
-
+Testprogram proge = new;
 L_CPU my_cpu(reset, clk, Flags, debug, Data_to_ram_debug, Aadress_to_ram_debug);
 
 initial begin
@@ -48,8 +93,8 @@ initial begin
    Aadress_to_ram_debug <= 0;
    
 #10 reset<=0;
-laadimine1.insertCommand(17, 58);
-laadimine1.insertCommand(18, 25);
+
+proge.adding(3, 5);
 
 $display ("cpu flags value is %d" , Flags);
 
@@ -57,5 +102,4 @@ $display ("cpu flags value is %d" , Flags);
 end
 
 
-endmodule 
-//shalalala
+endmodule
